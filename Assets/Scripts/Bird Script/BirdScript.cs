380817aa5d78c6f2f9a1d91141be6fd745067e6e
excalibour <anourkha@ucsc.edu>
2016-03-16 19:02:04 -0700

    Initial Upload

---
+++ Assets/Scripts/Bird Script/BirdScript.cs
@@ -0,0 +1,198 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class BirdScript : MonoBehaviour {
+
+	public static BirdScript instance;
+
+	[SerializeField]
+	private Rigidbody2D myRigidBody;
+
+	[SerializeField]
+	private Animator anim;
+
+	private float forwardSpeed = 3f; //3f;    5.5f(green)     3f(red and blue)
+
+    private float bounceSpeed = 4f; //4f;       6f (red)        4f(green and blue)
+    //if (PlayerPrefs.IsGreenBirdUnlocked())
+
+        
+
+	private bool didFlap;
+
+	public bool isAlive;
+
+	private Button flapButton;
+
+	[SerializeField]
+	private AudioSource audioSource;
+
+	[SerializeField]
+	private AudioClip flapClick, pointClip, diedClip;
+
+	public int score;
+
+	void Awake() {
+		if (instance == null) {
+			instance = this;
+		}
+
+		isAlive = true;
+		score = 0;
+
+		flapButton = GameObject.FindGameObjectWithTag ("FlapButton").GetComponent<Button> ();
+		flapButton.onClick.AddListener (() => FlapTheBird());
+
+		SetCamerasX ();
+	}
+
+	// Use this for initialization
+	void Start () {
+        if (GameController.instance.GetSelectedBird() == 1)
+        {
+            forwardSpeed = 5.5f;
+            bounceSpeed = 3.3f;
+        }
+
+        if (GameController.instance.GetSelectedBird() == 2)
+        {
+            forwardSpeed = 2.3f;
+            bounceSpeed = 6f;
+
+        }
+
+        //if (MenuController.copyOfBirds[1].activeSelf)
+        //    forwardSpeed = 6f;
+        //if (MenuController.copyOfBirds[2].activeSelf)
+        //    bounceSpeed = 8f;
+
+
+      /*  if (GameController.instance.IsGreenBirdUnlocked() == 1)
+            forwardSpeed = 6f;
+        if (GameController.instance.IsRedBirdUnlocked() == 1)
+            bounceSpeed = 8f;
+	
+       */ 
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+       // if (GameController.instance.//,IsGreenBirdUnlocked() )
+		if (isAlive) {
+            
+			Vector3 temp = transform.position;
+			temp.x += forwardSpeed * Time.deltaTime;
+			transform.position = temp;
+
+			if(didFlap) {
+				didFlap = false;
+				myRigidBody.velocity = new Vector2(0, bounceSpeed);
+				audioSource.PlayOneShot(flapClick);
+				anim.SetTrigger("Flap");
+			}
+
+			if(myRigidBody.velocity.y >= 0) {
+				transform.rotation = Quaternion.Euler(0, 0, 0);
+			} else {
+				float angle = 0;
+				angle = Mathf.Lerp(0, -90, -myRigidBody.velocity.y / 7);
+				transform.rotation = Quaternion.Euler(0, 0, angle);
+			}
+
+		}
+
+	}
+
+	void SetCamerasX() {
+		CameraScript.offsetX = (Camera.main.transform.position.x - transform.position.x) - 1f;
+	}
+
+	public float GetPositionX() {
+		return transform.position.x;
+	}
+
+	public void FlapTheBird() {
+		didFlap = true;
+	}
+
+	void OnCollisionEnter2D(Collision2D target) {
+		if (target.gameObject.tag == "Ground" || target.gameObject.tag == "Pipe") {
+			if(isAlive) {
+				isAlive = false;
+				anim.SetTrigger("Bird Died");
+				audioSource.PlayOneShot(diedClip);
+				GameplayController.instance.PlayerDiedShowScore(score);
+                int random = Random.Range(0, 10);
+                if (random > 5)
+				AdsController.instance.ShowInterstitial();
+			}
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D target) {
+		if (target.tag == "PipeHolder") {
+			score++;
+			GameplayController.instance.SetScore(score);
+			audioSource.PlayOneShot(pointClip);
+		}
+	}
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Assets/Scripts/Game Controllers/SocialMediaController.cs
@@ -0,0 +1,273 @@
+﻿using UnityEngine;
+using System.Collections;
+using Soomla;
+using Soomla.Profile;
+
+public class SocialMediaController : MonoBehaviour
+{
+
+    public static SocialMediaController instance;
+
+    void Awake()
+    {
+        MakeSingleton();
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        //SoomlaProfileAndroid.Initialize();
+        SoomlaProfile.Initialize();
+    }
+
+    public void OnEnable()
+    {
+
+        ProfileEvents.OnLoginFinished += onLoginFinished;
+        ProfileEvents.OnLoginFailed += onLoginFailed;
+        ProfileEvents.OnLoginCancelled += onLoginCancelled;
+        ProfileEvents.OnLogoutFinished += onLogoutFinished;
+        ProfileEvents.OnLogoutFailed += onLogoutFailed;
+        ProfileEvents.OnSocialActionFinished += onSocialActionFinished;
+        ProfileEvents.OnSocialActionFailed += onSocialActionFailed;
+        ProfileEvents.OnSocialActionCancelled += onSocialActionCancelled;
+    }
+
+    public void OnDisable()
+    {
+        ProfileEvents.OnLoginFinished -= onLoginFinished;
+        ProfileEvents.OnLoginFailed -= onLoginFailed;
+        ProfileEvents.OnLoginCancelled -= onLoginCancelled;
+        ProfileEvents.OnLogoutFinished -= onLogoutFinished;
+        ProfileEvents.OnLogoutFailed -= onLogoutFailed;
+        ProfileEvents.OnSocialActionFinished -= onSocialActionFinished;
+        ProfileEvents.OnSocialActionFailed -= onSocialActionFailed;
+        ProfileEvents.OnSocialActionCancelled -= onSocialActionCancelled;
+    }
+
+    void MakeSingleton()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    public void LogInOrLogOutTwitter()
+    {
+        if (SoomlaProfile.IsLoggedIn(Provider.TWITTER))
+        {
+            SoomlaProfile.Logout(Provider.TWITTER);
+        }
+        else
+        {
+            SoomlaProfile.Login(Provider.TWITTER);
+        }
+    }
+
+    public void LogInOrLogOutFacebook()
+    {
+        if (SoomlaProfile.IsLoggedIn(Provider.FACEBOOK))
+        {
+            SoomlaProfile.Logout(Provider.FACEBOOK);
+        }
+        else
+        {
+            SoomlaProfile.Login(Provider.FACEBOOK);
+        }
+    }
+
+    public void ShareOnTwitter()
+    {
+        if (SoomlaProfile.IsLoggedIn(Provider.TWITTER))
+        {
+
+            SoomlaProfile.UpdateStory(
+                Provider.TWITTER,
+                "Testing twitter connectivity... " + Random.Range(0, 100),
+                null,
+                null,
+                null,
+                "www.Link.com",
+                null,
+                null,
+                null
+                );
+        }
+        else
+        {
+            if (Application.loadedLevelName == "MainMenu")
+            {
+                MenuController.instance.NotificationMessage("Please Connect in Order to Post");
+            }
+        }
+    }
+
+
+    public void ShareToFacebook()
+    {
+        if (SoomlaProfile.IsLoggedIn(Provider.FACEBOOK))
+        {
+
+            SoomlaProfile.UpdateStatus(
+                Provider.FACEBOOK,
+                "Testing Facebook connectivity... " + Random.Range(0, 100),
+                null,
+                null
+                );
+        }
+        else
+        {
+            if (Application.loadedLevelName == "MainMenu")
+            {
+                MenuController.instance.NotificationMessage("Please Connect in Order to Post to Facebook");
+            }
+        }
+    }
+
+    void onLoginFinished(UserProfile userProfileJson, bool s, string payload)
+    {
+        if (Application.loadedLevelName == "MainMenu")
+        {
+            MenuController.instance.NotificationMessage("Connected");
+        }
+    }
+
+    void onLoginFailed(Provider provider, string message, bool s, string payload)
+    {
+        if (Application.loadedLevelName == "MainMenu")
+        {
+            MenuController.instance.NotificationMessage("Log In Failed");
+        }
+    }
+
+    void onLoginCancelled(Provider provider, bool s, string payload)
+    {
+        if (Application.loadedLevelName == "MainMenu")
+        {
+            MenuController.instance.NotificationMessage("Log In Canceled");
+        }
+    }
+
+    void onLogoutFinished(Provider provider)
+    {
+        if (Application.loadedLevelName == "MainMenu")
+        {
+            MenuController.instance.NotificationMessage("Disconnected");
+        }
+    }
+
+    void onLogoutFailed(Provider provider, string message)
+    {
+        if (Application.loadedLevelName == "MainMenu")
+        {
+            MenuController.instance.NotificationMessage("Could Not Disconnect");
+        }
+    }
+
+    void onSocialActionFinished(Provider provider, SocialActionType action, string payload)
+    {
+        if (provider == Provider.TWITTER)
+        {
+            if (action == SocialActionType.UPDATE_STORY)
+            {
+                if (Application.loadedLevelName == "MainMenu")
+                {
+                    MenuController.instance.NotificationMessage("Thank You For Sharing");
+                }
+            }
+        }
+    }
+
+    void onSocialActionCancelled(Provider provider, SocialActionType action, string payload)
+    {
+        if (provider == Provider.TWITTER)
+        {
+            if (action == SocialActionType.UPDATE_STORY)
+            {
+                if (Application.loadedLevelName == "MainMenu")
+                {
+                    MenuController.instance.NotificationMessage("Could Not Post");
+                }
+            }
+        }
+    }
+
+    void onSocialActionFailed(Provider provider, SocialActionType action, string message, string payload)
+    {
+        if (provider == Provider.TWITTER)
+        {
+            if (action == SocialActionType.UPDATE_STORY)
+            {
+                if (Application.loadedLevelName == "MainMenu")
+                {
+                    MenuController.instance.NotificationMessage("Could Not Post");
+                }
+            }
+        }
+    }
+
+} // class
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
